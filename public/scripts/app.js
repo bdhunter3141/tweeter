372510894b5118261130d99d1b4b1c3699235823
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-19 23:55:47 +0000

    Now GETs and POSTs from/to '/tweets/' page.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 // Animated icons for hover over tweet
 
 $(document).ready(function() {
@@ -16,55 +17,21 @@ $(document).ready(function() {
   });
 
 
+  // Prevent the default submit of the new tweet form and post to "/tweets/"
 
-  // Fake data taken from tweets.json
+  $("#tweet-submit").submit(function(event) {
+    $.ajax({url: "/tweets/", dataType: "text", type: "post", data: $(this).serialize()});
+    event.preventDefault();
+  });
 
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
+
+  // Loads the tweets from the "/tweets/" page
+
+  let loadTweets = function() {
+    $.get("/tweets/", function(data){
+      renderTweets(data);
+    })
+  }();
 
 
   // Escapes text for safe use in tweets
@@ -101,47 +68,6 @@ $(document).ready(function() {
         </footer>
       </article>`;
 
-    // let $tweet = $("<article>")
-    //   .addClass("tweet");
-
-    // Create the header
-    // let $header = $("<header>")
-    //   .addClass("tweet-header");
-    // $header.append("<div>").addClass("avatar-div")
-    //   .append($("<img>")
-    //   .attr("src", tweetInfo.user.avatars.small)
-    //   .attr("alt", "Avatar").addClass("tweet-avatar"));
-    // $header.append($("<h1>")
-    //   .text(tweetInfo.user.name));
-    // $header.append($("<p>")
-    //   .text(tweetInfo.user.handle)
-    //   .addClass("header-handle"));
-    // $tweet.append($header);
-
-    // Create the body
-    // let $body = $("<div>")
-    //   .text(tweetInfo.content.text)
-    //   .addClass("tweet-content");
-    // $tweet.append($body);
-
-    // Create the footer
-    // let $footer = $("<footer>")
-    //   .addClass("tweet-footer");
-    // $footer.append($("<p>")
-    //   .text(Math.floor((Date.now() - tweetInfo.created_at) / 86400000)  + " days ago"));
-    // let $tweetIcons = $("<div>")
-    //   .addClass("tweet-icons");
-    // $tweetIcons.append($("<i>")
-    //   .addClass("fa fa-flag")
-    //   .attr("aria-hidden", "true"));
-    // $tweetIcons.append($("<i>")
-    //   .addClass("fa fa-retweet")
-    //   .attr("aria-hidden", "true"));
-    // $tweetIcons.append($("<i>")
-    //   .addClass("fa fa-heart")
-    //   .attr("aria-hidden", "true"));
-    // $footer.append($tweetIcons);
-    // $tweet.append($footer);
     return $tweet;
   }
 
@@ -154,10 +80,5 @@ $(document).ready(function() {
       $("#tweet-container").append($tweet);
     }
   }
-
-
-  renderTweets(data);
-
-
 
 });
